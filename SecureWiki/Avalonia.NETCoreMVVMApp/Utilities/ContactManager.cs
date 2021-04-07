8203192fd13255344070739d2b81048af64a9168
smousten <smousten@gmail.com>
2021-04-07 11:29:08 +0200

    Added contact list to revoke access popup. Tested and changed import contacts

--- SecureWiki/Avalonia.NETCoreMVVMApp/Utilities/ContactManager.cs
+++ SecureWiki/Avalonia.NETCoreMVVMApp/Utilities/ContactManager.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
@@ -144,7 +146,7 @@ namespace SecureWiki.Utilities
             var newList = MergeContactLists(new List<Contact>(OwnContacts), new List<Contact>(newContacts));
             
         }
-
+        
         private List<Contact> MergeContactLists(List<Contact> existingContacts, List<Contact> newContacts)
         {
             List<Contact> newInputList = new();
@@ -158,90 +160,145 @@ namespace SecureWiki.Utilities
             ownList.AddRange(existingContacts);
             ownList = ownList.OrderBy(entry => entry.PageTitle).ToList();
 
-            // Check if there are any collisions in regard to PageTitle and ServerLink
-            int l = 0;
-            foreach (var inputContact in inputList)
+            // If no existing contact, return new contacts sorted by nickname
+            if (existingContacts.Count == 0)
             {
-                while (true)
-                {
-                    if (l > ownList.Count)
-                    {
-                        break;
-                    }
-
-                    var comp = inputContact.PageTitle.CompareTo(ownList[l].PageTitle);
-                    
-                    if (comp > 0)
-                    {
-                        continue;
-                    }
-                    else if (comp < 0)
-                    {
-                        newInputList.Add(inputContact);
-                        break;
-                    }
-                    else
-                    {
-                        if (!inputContact.ServerLink.Equals(ownList[l].ServerLink))
-                        {
-                            continue;
-                        }
-                        else if (!inputContact.Nickname.Equals(ownList[l].Nickname))
-                        {
-                            string loggerMsg = $"Imported contact with nickname '{inputContact.Nickname}' " +
-                                               $"contains same information as existing contact with nickname " +
-                                               $"'{ownList[l].Nickname}' and will not be added to contacts.";
-                            manager.WriteToLogger(loggerMsg, null, LoggerEntry.LogPriority.Warning);
-                        }
-                    }
-                    
-                    l++;
-                }
+                resultingList = inputList.OrderBy(entry => entry.Nickname).ToList();
+                return resultingList;
             }
             
-            // Sort lists by nickname
-            newInputList = newInputList.OrderBy(entry => entry.Nickname).ToList();
-            ownList = ownList.OrderBy(entry => entry.Nickname).ToList();
-
-            // Check if there are any collisions in regard to Nickname
-            int k = 0;
-            foreach (var inputContact in newInputList)
+            // If no new contacts, return existing contacts sorted by nickname
+            if (newContacts.Count == 0)
             {
-                while (true)
-                {
-                    if (k > ownList.Count)
-                    {
-                        break;
-                    }
+                resultingList = ownList.OrderBy(entry => entry.Nickname).ToList();
+                return resultingList;
+            }
 
-                    var comp = inputContact.Nickname.CompareTo(ownList[k].Nickname);
-                    
-                    if (comp > 0)
+            // Resulting list must always contain existing contacts
+            resultingList.AddRange(ownList);
+            
+            // Check for collisions in pageTitle, serverLink, and nicknames
+            foreach (var contact in inputList)
+            {
+                // If no collisions in pageTitle, serverLink and nickname, 
+                // then add new contact to resulting list.
+                if (!resultingList.Exists(e => e.PageTitle.Equals(contact.PageTitle) &&
+                                         e.ServerLink.Equals(contact.ServerLink)))
+                {
+                    // If new contact nickname already exists in contact list,
+                    // then rename contact
+                    if (resultingList.Exists(e => e.Nickname.Equals(contact.Nickname)))
                     {
-                        continue;
-                    }
-                    else if (comp < 0)
-                    {
-                        resultingList.Add(inputContact);
-                        break;
-                    }
-                    else
-                    {
-                        inputContact.Nickname = inputContact.Nickname + "(1)"; 
-                        
-                        string loggerMsg = $"Imported contact with nickname '{ownList[k].Nickname}' " +
+                        var oldNickname = contact.Nickname;
+                        contact.Nickname = contact.Nickname + "(1)";
+                        string loggerMsg = $"Imported contact with nickname '{oldNickname}' " +
                                            $"contains different information compared to existing contact with same nickname " +
-                                           $"and will be renamed to '{inputContact.Nickname}'.";
+                                           $"and will be renamed to '{contact.Nickname}'.";
                         manager.WriteToLogger(loggerMsg, null, LoggerEntry.LogPriority.Warning);
+                        resultingList.Add(contact);
                     }
-                    
-                    k++;
+                    else
+                    {
+                        resultingList.Add(contact);
+                    }
+                }
+                // If collision in PageTitle and serverLink, then do not add new contact to resulting list
+                else
+                {
+                    string loggerMsg = $"Imported contact with nickname '{contact.Nickname}' " +
+                                       $"contains same information as existing contact " +
+                                       $"and will not be added to contacts.";
+                    manager.WriteToLogger(loggerMsg, null, LoggerEntry.LogPriority.Warning);
                 }
             }
-            
-            resultingList = resultingList.OrderBy(entry => entry.Nickname).ToList();
+
+            resultingList = resultingList.OrderBy(e => e.Nickname).ToList();
 
             return resultingList;
+            // Check if there are any collisions in regard to PageTitle and ServerLink
+            // int l = 0;
+            // foreach (var inputContact in inputList)
+            // {
+            //     while (true)
+            //     {
+            //         if (l > ownList.Count)
+            //         {
+            //             break;
+            //         }
+            //
+            //         var comp = inputContact.PageTitle.CompareTo(ownList[l].PageTitle);
+            //         
+            //         if (comp > 0)
+            //         {
+            //             continue;
+            //         }
+            //         else if (comp < 0)
+            //         {
+            //             newInputList.Add(inputContact);
+            //             break;
+            //         }
+            //         else
+            //         {
+            //             if (!inputContact.ServerLink.Equals(ownList[l].ServerLink))
+            //             {
+            //                 continue;
+            //             }
+            //             else if (!inputContact.Nickname.Equals(ownList[l].Nickname))
+            //             {
+            //                 string loggerMsg = $"Imported contact with nickname '{inputContact.Nickname}' " +
+            //                                    $"contains same information as existing contact with nickname " +
+            //                                    $"'{ownList[l].Nickname}' and will not be added to contacts.";
+            //                 manager.WriteToLogger(loggerMsg, null, LoggerEntry.LogPriority.Warning);
+            //             }
+            //         }
+            //         
+            //         l++;
+            //     }
+            // }
+            //
+            // // Sort lists by nickname
+            // newInputList = newInputList.OrderBy(entry => entry.Nickname).ToList();
+            // ownList = ownList.OrderBy(entry => entry.Nickname).ToList();
+            //
+            // // Check if there are any collisions in regard to Nickname
+            // int k = 0;
+            // foreach (var inputContact in newInputList)
+            // {
+            //     while (true)
+            //     {
+            //         if (k > ownList.Count)
+            //         {
+            //             break;
+            //         }
+            //
+            //         var comp = inputContact.Nickname.CompareTo(ownList[k].Nickname);
+            //         
+            //         if (comp > 0)
+            //         {
+            //             continue;
+            //         }
+            //         else if (comp < 0)
+            //         {
+            //             resultingList.Add(inputContact);
+            //             break;
+            //         }
+            //         else
+            //         {
+            //             inputContact.Nickname = inputContact.Nickname + "(1)"; 
+            //             
+            //             string loggerMsg = $"Imported contact with nickname '{ownList[k].Nickname}' " +
+            //                                $"contains different information compared to existing contact with same nickname " +
+            //                                $"and will be renamed to '{inputContact.Nickname}'.";
+            //             manager.WriteToLogger(loggerMsg, null, LoggerEntry.LogPriority.Warning);
+            //         }
+            //         
+            //         k++;
+            //     }
+            // }
+            //
+            // resultingList = resultingList.OrderBy(entry => entry.Nickname).ToList();
+            //
+            // return resultingList;
         }
 
         public void SortContacts()
